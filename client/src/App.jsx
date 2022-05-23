62aba08b307e3777e2566710d3e6e22d4399a5c3
M Shashanka <shashanka318@gmail.com>
2022-05-23 07:39:09 +0530

    add header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,11 @@
-import {useState,useEffect} from "react";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [data,setData] = useState("Testing");
-
-  useEffect(()=>{
-    const getData = async() =>{
-      try{
-        const res = await fetch("http://localhost:8080/test");
-        return res.json();
-      } catch(err){
-        console.log(err);
-      }
-    }
-    getData().then((res)=>{
-      setData(res.message);
-    }).catch((err)=> console.log(err));
-  },[]);
 
   return (
-    <h1>{data}</h1>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import styles from './header.module.css';
+
+function Header() {
+    return (
+        <header>
+            <img className={styles.logo1} src="http://d2e9h3gjmozu47.cloudfront.net/brand.png" alt="logo1-img" />
+            <h1>Data Structures LAB</h1>
+            <img className={styles.logo2} src="./icon.png" alt="logo2-img" />
+        </header>
+    );
+}
+
+export default Header;
