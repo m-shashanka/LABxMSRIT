2714f9932607482963753afd3b886f3d27deb1fd
Shashank <shashanka318@gmail.com>
2022-07-07 13:04:14 +0530

    add home redirection

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './header.module.css';
 
 function Header({name}) {
@@ -5,7 +6,9 @@ function Header({name}) {
         <header className={styles.dslab}>
             <img className={styles.logo1} src="http://d2e9h3gjmozu47.cloudfront.net/brand.png" alt="logo1-img" />
             <h1>{name}</h1>
-            <img className={styles.logo2} src="./icon.png" alt="logo2-img" />
+            <Link to={`/`} style={{ all: 'unset' }}>
+                <img className={styles.logo2} src="./icon.png" alt="logo2-img" />
+            </Link>
         </header>
     );
 }
